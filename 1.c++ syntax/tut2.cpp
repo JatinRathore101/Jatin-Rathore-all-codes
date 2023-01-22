3dc6a4f934aaddf4c39b2f6cac52bcd93ffecca0
Jatin Rathore <sbi.kindle.aadhaar.jatin@gmail.com>
2023-01-23 00:29:32 +0530

    commit 2

---
+++ 1.c++ syntax/tut2.cpp
@@ -0,0 +1,16 @@
+ #include<iostream>
+
+// single line comment
+
+/*multiline
+he
+ha
+ha
+comment*/
+
+using namespace std;
+int main(){
+    int sum=7;
+    cout<<"I am Jatin "<<sum;
+    return 0;
+}
